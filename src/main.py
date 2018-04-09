2f75b7e738d516795e8c375ffb1b31dbed1b3ba0
Tomas Patro <tomas.patro@gmail.com>
2018-04-09 20:34:45 +0200

    [Edit][main.py] Edited console output from the server side.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Sample TCP/IP server"""
 import socket
+BUFFER_SIZE = 32
 def main():
     """Sample TCP/IP connection from server side"""
 
@@ -9,7 +10,7 @@ def main():
 
     # Bind the socket to the port
     server_address = ('localhost', 10000)
-    print('starting up on {} port {}'.format(*server_address))
+    print('Starting up on {}, port: {}'.format(*server_address))
     sock.bind(server_address)
 
     # Listen for incoming connections
@@ -17,20 +18,21 @@ def main():
 
     while True:
         # Wait for a connection
-        print('waiting for a connection')
+        print('Waiting for a connection...')
         connection, client_address = sock.accept()
         try:
-            print('connection from', client_address)
+            print('Connection from:', client_address)
 
-            # Receive the data in small chunks and retransmit it
+            # (BUFFER_SIZE)Receive the data in
+            #  small chunks and retransmit it
             while True:
-                data = connection.recv(16)
-                print('received {!r}'.format(data))
+                data = connection
+                print('Received: {!r}'.format(data))
                 if data:
-                    print('sending data back to the client')
+                    print('Sending data back to the client...')
                     connection.sendall(data)
                 else:
-                    print('no data from', client_address)
+                    print('No data from:', client_address)
                     break
 
         finally:
