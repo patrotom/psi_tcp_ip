43f833c118fafbe70c8737414404a81c0fefee73
Tomas Patro <tomas.patro@gmail.com>
2018-04-10 22:05:21 +0200

    [Edit][main.py] Defining the main logic for the communication between a client and the server

--- src/main.py
+++ src/main.py
@@ -2,6 +2,24 @@
 """Sample TCP/IP server"""
 import socket
 BUFFER_SIZE = 32
+# Spravy mozu byt odosielane naraz, nemusi prist cela sprava naraz, takze sa musim vzdy pozerat na validnu spravu pomocou delimiterov. Rozdelit si logiku na viac vrstiev a oddelit si prijmanie rechargingu a dalsich 3 typov sprav od klienta.
+def login(connection, client_address):
+    """Function which authenticates a new robot"""
+    msg_count = 0
+    while True:
+        data = connection.recv(16)
+        raw_data = data.decode('ascii')
+        
+        print('Received:', raw_data)
+
+        if data:
+            print('Sending data back to the client...')
+            connection.sendall(data)
+        else:
+            print('No data from:', client_address)
+            break
+        
+
 def main():
     """Sample TCP/IP connection from server side"""
 
@@ -20,22 +38,7 @@ def main():
         # Wait for a connection
         print('Waiting for a connection...')
         connection, client_address = sock.accept()
-        try:
-            print('Connection from:', client_address)
-
-            # (BUFFER_SIZE)Receive the data in
-            #  small chunks and retransmit it
-            while True:
-                data = connection
-                print('Received: {!r}'.format(data))
-                if data:
-                    print('Sending data back to the client...')
-                    connection.sendall(data)
-                else:
-                    print('No data from:', client_address)
-                    break
-
-        finally:
-            # Clean up the connection
-            connection.close()
+        login(connection, client_address)
+        # Clean up the connection
+        connection.close()
 main()
