f92761a5b69e80ea75cb5144f53f2abe1f474036
Tomas Patro <tomas.patro@gmail.com>
2018-04-16 11:53:04 +0200

    [Edit][main.py] Added parsing of the messages via delimiters which is now working. Need to add also checking of the validity of the messages.

--- src/main.py
+++ src/main.py
@@ -1,43 +1,57 @@
 #!/usr/bin/python3
-"""Sample TCP/IP server"""
+'''Sample TCP/IP server'''
 import socket
 BUFFER_SIZE = 32
-# Spravy mozu byt odosielane naraz, nemusi prist cela sprava naraz, takze sa musim vzdy pozerat na validnu spravu pomocou delimiterov. Rozdelit si logiku na viac vrstiev a oddelit si prijmanie rechargingu a dalsich 3 typov sprav od klienta.
-def login(connection, client_address):
-    """Function which authenticates a new robot"""
-    msg_count = 0
-    while True:
-        data = connection.recv(16)
-        raw_data = data.decode('ascii')
-        
-        print('Received:', raw_data)
 
-        if data:
-            print('Sending data back to the client...')
-            connection.sendall(data)
-        else:
-            print('No data from:', client_address)
-            break
+class Handler:
+    def __init__(self, connection, client_address):
+        '''Constructor'''
+        self.connection = connection
+        self.client_address = client_address
+        self.raw_data = ''
+        self.buffer = ''
+
+    def Listen(self):
+        '''Method that reads data from the buffer'''
+        while True:
+            data = self.connection.recv(BUFFER_SIZE)
+            
+            if data:
+                raw_data = data.decode('ascii')
+                print('Received:', raw_data)
+                self.buffer += raw_data
+            if not self.Parse():
+                break
+    
+    def Parse(self):
+        '''Method that is parsing messages from the input buffer'''
+        message = self.buffer.partition('\\a\\b')
+        if message[1]:
+            self.buffer = message[2]
+            return self.EvaluateMessage(message[0])
+        return False
+
+    def EvaluateMessage(self, message):
+        '''Method that is evaluating messages'''
+        print(message)
+        if message == 'close':
+            return False
+        return True
 
 def main():
-    """Sample TCP/IP connection from server side"""
-
-    # Create a TCP/IP socket
+    '''Sample TCP/IP connection from server side'''
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to the port
     server_address = ('localhost', 10000)
     print('Starting up on {}, port: {}'.format(*server_address))
     sock.bind(server_address)
 
-    # Listen for incoming connections
     sock.listen(1)
 
     while True:
-        # Wait for a connection
         print('Waiting for a connection...')
         connection, client_address = sock.accept()
-        login(connection, client_address)
-        # Clean up the connection
+        c = Handler(connection, client_address)
+        c.Listen()
         connection.close()
 main()
